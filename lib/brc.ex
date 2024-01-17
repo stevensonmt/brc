7559cb61315fb8e2f1f51eb1439c91c112af4731
matt <stevensonmt@gmail.com>
2024-01-17 09:49:02 -0800

    pattern match binary for line processing, write_concurrency true for table

--- lib/brc.ex
+++ lib/brc.ex
@@ -1,9 +1,9 @@
 defmodule Brc do
   @pool_size :erlang.system_info(:logical_processors)
-  @blob_size 100_000
+  @blob_size 1024 * 1024
+  @max_rows 50_000_000
+  @chunk_size :math.sqrt(@max_rows) |> ceil()
   @table :weather_stations
-  @splitters [";", "\n"]
-  @split_opts [:global, :trim_all]
 
   def process_stream(stream) do
     for line <- stream do
@@ -11,11 +11,81 @@ defmodule Brc do
     end
   end
 
-  def process_line(line) do
-    [city, temp] = :binary.split(line, @splitters, @split_opts)
+  def process_line(""), do: ""
 
-    {temp, ""} = temp |> Float.parse()
+  def process_line(<<city::208, ";", temp::binary>>),
+    do: update_table(<<city::208>>, Float.parse(temp))
 
+  def process_line(<<city::200, ";", temp::binary>>),
+    do: update_table(<<city::200>>, Float.parse(temp))
+
+  def process_line(<<city::192, ";", temp::binary>>),
+    do: update_table(<<city::192>>, Float.parse(temp))
+
+  def process_line(<<city::184, ";", temp::binary>>),
+    do: update_table(<<city::184>>, Float.parse(temp))
+
+  def process_line(<<city::176, ";", temp::binary>>),
+    do: update_table(<<city::176>>, Float.parse(temp))
+
+  def process_line(<<city::168, ";", temp::binary>>),
+    do: update_table(<<city::168>>, Float.parse(temp))
+
+  def process_line(<<city::160, ";", temp::binary>>),
+    do: update_table(<<city::160>>, Float.parse(temp))
+
+  def process_line(<<city::152, ";", temp::binary>>),
+    do: update_table(<<city::152>>, Float.parse(temp))
+
+  def process_line(<<city::144, ";", temp::binary>>),
+    do: update_table(<<city::144>>, Float.parse(temp))
+
+  def process_line(<<city::136, ";", temp::binary>>),
+    do: update_table(<<city::136>>, Float.parse(temp))
+
+  def process_line(<<city::128, ";", temp::binary>>),
+    do: update_table(<<city::128>>, Float.parse(temp))
+
+  def process_line(<<city::120, ";", temp::binary>>),
+    do: update_table(<<city::120>>, Float.parse(temp))
+
+  def process_line(<<city::112, ";", temp::binary>>),
+    do: update_table(<<city::112>>, Float.parse(temp))
+
+  def process_line(<<city::104, ";", temp::binary>>),
+    do: update_table(<<city::104>>, Float.parse(temp))
+
+  def process_line(<<city::96, ";", temp::binary>>),
+    do: update_table(<<city::96>>, Float.parse(temp))
+
+  def process_line(<<city::88, ";", temp::binary>>),
+    do: update_table(<<city::88>>, Float.parse(temp))
+
+  def process_line(<<city::80, ";", temp::binary>>),
+    do: update_table(<<city::80>>, Float.parse(temp))
+
+  def process_line(<<city::72, ";", temp::binary>>),
+    do: update_table(<<city::72>>, Float.parse(temp))
+
+  def process_line(<<city::64, ";", temp::binary>>),
+    do: update_table(<<city::64>>, Float.parse(temp))
+
+  def process_line(<<city::56, ";", temp::binary>>),
+    do: update_table(<<city::56>>, Float.parse(temp))
+
+  def process_line(<<city::48, ";", temp::binary>>),
+    do: update_table(<<city::48>>, Float.parse(temp))
+
+  def process_line(<<city::40, ";", temp::binary>>),
+    do: update_table(<<city::40>>, Float.parse(temp))
+
+  def process_line(<<city::32, ";", temp::binary>>),
+    do: update_table(<<city::32>>, Float.parse(temp))
+
+  def process_line(<<city::24, ";", temp::binary>>),
+    do: update_table(<<city::24>>, Float.parse(temp))
+
+  def update_table(city, {temp, _}) do
     temps =
       case :ets.lookup(@table, city) do
         [] ->
@@ -29,20 +99,20 @@ defmodule Brc do
   end
 
   def run_file(filename) do
-    :ets.new(@table, [:ordered_set, :public, :named_table])
-
-    streams = File.stream!(filename, read_ahead: @blob_size) |> Stream.chunk_every(@blob_size)
-
-    streams
+    filename
+    |> File.stream!(read_ahead: @blob_size)
+    |> Stream.chunk_every(@chunk_size)
     |> Task.async_stream(fn stream -> process_stream(stream) end,
       max_concurrency: @pool_size,
       timeout: :infinity
     )
     |> Stream.run()
+  end
 
-    processed =
-      :ets.tab2list(@table)
-      |> Enum.map(fn {city, {_len, min, max, mean}} ->
+  def print_table(table) do
+    out =
+      :ets.tab2list(table)
+      |> Task.async_stream(fn {city, {_len, min, max, mean}} ->
         city <>
           "=" <>
           :erlang.float_to_binary(min, decimals: 1) <>
@@ -50,18 +120,24 @@ defmodule Brc do
           :erlang.float_to_binary(mean, decimals: 1) <>
           "/" <> :erlang.float_to_binary(max, decimals: 1)
       end)
+      |> Enum.map(&elem(&1, 1))
+      |> Enum.sort()
       |> Enum.join(",")
 
-    IO.puts("{#{processed}}")
+    IO.puts("{#{out}}")
   end
 
   def main(args) do
+    :ets.new(@table, [:set, :public, :named_table, write_concurrency: true])
+
     {uSec, :ok} =
       :timer.tc(fn ->
         run_file(Enum.at(args, 0))
+        print_table(@table)
         :ok
       end)
 
     IO.puts("It took #{uSec / 1000} milliseconds")
+    :ets.delete(@table)
   end
 end
